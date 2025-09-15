f575e30dc27c88169a9feefbff52cee6325ed2aa
Ahmed Awan <qe66653@umbc.edu>
2025-09-15 12:28:20 -0500

    reorder release announcement steps (need to prepare first then announce)

--- galaxy_release_util/bootstrap_history.py
+++ galaxy_release_util/bootstrap_history.py
@@ -346,12 +346,12 @@ RELEASE_ISSUE_TEMPLATE = string.Template(
 
     - [ ] Verify release included in https://docs.galaxyproject.org/en/master/releases/index.html.
     - [ ] Review announcement in https://github.com/galaxyproject/galaxy/blob/dev/doc/source/releases/${version}_announce.rst.
+    - [ ] Adjust http://getgalaxy.org text and links to match current master branch by opening a PR at https://github.com/galaxyproject/galaxy-hub/
+    - [ ] Add gxy.io link to notes (e.g.: https://github.com/galaxyproject/gxy.io/pull/76)
     - [ ] Announce release on [Galaxy Hub](https://galaxyproject.org/) not as a full news content item, but rather as a `external_url` link to the user facing release notes.
     - [ ] Post announcement to [Galaxy Help](https://help.galaxyproject.org/). [An example](https://help.galaxyproject.org/t/release-of-galaxy-23-2/11675).
     - [ ] Announce release on Galaxy's social media accounts ([Bluesky](https://bsky.app/profile/galaxyproject.bsky.social), [Mastodon](https://mstdn.science/@galaxyproject), [LinkedIn](https://linkedin.com/company/galaxy-project)).
     - [ ] Email announcement to [galaxy-dev](http://dev.list.galaxyproject.org/) and [galaxy-announce](http://announce.list.galaxyproject.org/) @lists.galaxyproject.org. [An example](https://lists.galaxyproject.org/archives/list/galaxy-announce@lists.galaxyproject.org/thread/ISB7ZNBDY3LQMC2KALGPVQ3DEJTH657Q/).
-    - [ ] Adjust http://getgalaxy.org text and links to match current master branch by opening a PR at https://github.com/galaxyproject/galaxy-hub/
-    - [ ] Add gxy.io link to notes (e.g.: https://github.com/galaxyproject/gxy.io/pull/76)
 
 - [ ] **Complete release**
 
